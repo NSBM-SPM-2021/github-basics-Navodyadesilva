d3eabb3bd52994df1d3fc7355007db04dae6faa6
Navodyadesilva <71263012+Navodyadesilva@users.noreply.github.com>
2021-10-04 23:20:36 +0530

    changed

--- EvaluationSystem/frmStudent.Designer.cs
+++ EvaluationSystem/frmStudent.Designer.cs
@@ -71,7 +71,7 @@
             "Second",
             "Third",
             "Fourth"});
-            this.cboYearLevel.Location = new System.Drawing.Point(377, 241);
+            this.cboYearLevel.Location = new System.Drawing.Point(449, 241);
             this.cboYearLevel.Name = "cboYearLevel";
             this.cboYearLevel.Size = new System.Drawing.Size(100, 21);
             this.cboYearLevel.TabIndex = 126;
@@ -81,7 +81,7 @@
             // 
             this.cboCourse.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.cboCourse.FormattingEnabled = true;
-            this.cboCourse.Location = new System.Drawing.Point(211, 241);
+            this.cboCourse.Location = new System.Drawing.Point(283, 241);
             this.cboCourse.Name = "cboCourse";
             this.cboCourse.Size = new System.Drawing.Size(160, 21);
             this.cboCourse.TabIndex = 125;
@@ -91,7 +91,7 @@
             // 
             this.btnClose.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.btnClose.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClose.Location = new System.Drawing.Point(735, 206);
+            this.btnClose.Location = new System.Drawing.Point(807, 206);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(68, 36);
             this.btnClose.TabIndex = 121;
@@ -103,7 +103,7 @@
             // 
             this.btn_last.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.btn_last.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_last.Location = new System.Drawing.Point(716, 248);
+            this.btn_last.Location = new System.Drawing.Point(788, 248);
             this.btn_last.Name = "btn_last";
             this.btn_last.Size = new System.Drawing.Size(51, 24);
             this.btn_last.TabIndex = 119;
@@ -115,7 +115,7 @@
             // 
             this.btn_next.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.btn_next.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_next.Location = new System.Drawing.Point(659, 248);
+            this.btn_next.Location = new System.Drawing.Point(731, 248);
             this.btn_next.Name = "btn_next";
             this.btn_next.Size = new System.Drawing.Size(51, 24);
             this.btn_next.TabIndex = 118;
@@ -127,7 +127,7 @@
             // 
             this.btn_New.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.btn_New.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_New.Location = new System.Drawing.Point(661, 207);
+            this.btn_New.Location = new System.Drawing.Point(733, 207);
             this.btn_New.Name = "btn_New";
             this.btn_New.Size = new System.Drawing.Size(68, 36);
             this.btn_New.TabIndex = 116;
@@ -138,7 +138,7 @@
             // 
             this.btn_first.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.btn_first.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_first.Location = new System.Drawing.Point(545, 248);
+            this.btn_first.Location = new System.Drawing.Point(617, 248);
             this.btn_first.Name = "btn_first";
             this.btn_first.Size = new System.Drawing.Size(51, 24);
             this.btn_first.TabIndex = 120;
@@ -150,7 +150,7 @@
             // 
             this.btn_delete.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.btn_delete.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_delete.Location = new System.Drawing.Point(587, 206);
+            this.btn_delete.Location = new System.Drawing.Point(659, 206);
             this.btn_delete.Name = "btn_delete";
             this.btn_delete.Size = new System.Drawing.Size(68, 36);
             this.btn_delete.TabIndex = 115;
@@ -162,7 +162,7 @@
             // 
             this.btn_save.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.btn_save.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_save.Location = new System.Drawing.Point(518, 206);
+            this.btn_save.Location = new System.Drawing.Point(590, 206);
             this.btn_save.Name = "btn_save";
             this.btn_save.Size = new System.Drawing.Size(66, 36);
             this.btn_save.TabIndex = 113;
@@ -174,7 +174,7 @@
             // 
             this.btn_prev.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.btn_prev.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_prev.Location = new System.Drawing.Point(602, 248);
+            this.btn_prev.Location = new System.Drawing.Point(674, 248);
             this.btn_prev.Name = "btn_prev";
             this.btn_prev.Size = new System.Drawing.Size(51, 24);
             this.btn_prev.TabIndex = 117;
@@ -187,7 +187,7 @@
             this.Label6.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.Label6.AutoSize = true;
             this.Label6.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Label6.Location = new System.Drawing.Point(118, 245);
+            this.Label6.Location = new System.Drawing.Point(190, 245);
             this.Label6.Name = "Label6";
             this.Label6.Size = new System.Drawing.Size(86, 17);
             this.Label6.TabIndex = 112;
@@ -210,7 +210,7 @@
             this.dtg_ABorrowLists.Name = "dtg_ABorrowLists";
             this.dtg_ABorrowLists.RowHeadersVisible = false;
             this.dtg_ABorrowLists.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dtg_ABorrowLists.Size = new System.Drawing.Size(877, 249);
+            this.dtg_ABorrowLists.Size = new System.Drawing.Size(1021, 249);
             this.dtg_ABorrowLists.TabIndex = 111;
             this.dtg_ABorrowLists.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtg_ABorrowLists_CellClick);
             // 
@@ -218,7 +218,7 @@
             // 
             this.txt_sid.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.txt_sid.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_sid.Location = new System.Drawing.Point(211, 31);
+            this.txt_sid.Location = new System.Drawing.Point(283, 31);
             this.txt_sid.Name = "txt_sid";
             this.txt_sid.Size = new System.Drawing.Size(278, 25);
             this.txt_sid.TabIndex = 97;
@@ -228,7 +228,7 @@
             // 
             this.txt_fname.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.txt_fname.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_fname.Location = new System.Drawing.Point(211, 64);
+            this.txt_fname.Location = new System.Drawing.Point(283, 64);
             this.txt_fname.Name = "txt_fname";
             this.txt_fname.Size = new System.Drawing.Size(278, 25);
             this.txt_fname.TabIndex = 98;
@@ -238,7 +238,7 @@
             this.rdio_female.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.rdio_female.AutoSize = true;
             this.rdio_female.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rdio_female.Location = new System.Drawing.Point(280, 214);
+            this.rdio_female.Location = new System.Drawing.Point(352, 214);
             this.rdio_female.Name = "rdio_female";
             this.rdio_female.Size = new System.Drawing.Size(67, 21);
             this.rdio_female.TabIndex = 109;
@@ -250,7 +250,7 @@
             // 
             this.txt_mname.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.txt_mname.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_mname.Location = new System.Drawing.Point(211, 122);
+            this.txt_mname.Location = new System.Drawing.Point(283, 122);
             this.txt_mname.Name = "txt_mname";
             this.txt_mname.Size = new System.Drawing.Size(278, 25);
             this.txt_mname.TabIndex = 100;
@@ -260,7 +260,7 @@
             this.Label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.Label1.AutoSize = true;
             this.Label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Label1.Location = new System.Drawing.Point(123, 34);
+            this.Label1.Location = new System.Drawing.Point(195, 34);
             this.Label1.Name = "Label1";
             this.Label1.Size = new System.Drawing.Size(74, 17);
             this.Label1.TabIndex = 103;
@@ -270,7 +270,7 @@
             // 
             this.txt_lname.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.txt_lname.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_lname.Location = new System.Drawing.Point(211, 94);
+            this.txt_lname.Location = new System.Drawing.Point(283, 94);
             this.txt_lname.Name = "txt_lname";
             this.txt_lname.Size = new System.Drawing.Size(278, 25);
             this.txt_lname.TabIndex = 99;
@@ -280,7 +280,7 @@
             this.Label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.Label2.AutoSize = true;
             this.Label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Label2.Location = new System.Drawing.Point(127, 67);
+            this.Label2.Location = new System.Drawing.Point(199, 67);
             this.Label2.Name = "Label2";
             this.Label2.Size = new System.Drawing.Size(78, 17);
             this.Label2.TabIndex = 106;
@@ -290,7 +290,7 @@
             // 
             this.rch_address.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.rch_address.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rch_address.Location = new System.Drawing.Point(211, 150);
+            this.rch_address.Location = new System.Drawing.Point(283, 150);
             this.rch_address.Name = "rch_address";
             this.rch_address.Size = new System.Drawing.Size(278, 58);
             this.rch_address.TabIndex = 102;
@@ -301,7 +301,7 @@
             this.Label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.Label3.AutoSize = true;
             this.Label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Label3.Location = new System.Drawing.Point(126, 97);
+            this.Label3.Location = new System.Drawing.Point(198, 97);
             this.Label3.Name = "Label3";
             this.Label3.Size = new System.Drawing.Size(77, 17);
             this.Label3.TabIndex = 104;
@@ -312,7 +312,7 @@
             this.rdio_male.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.rdio_male.AutoSize = true;
             this.rdio_male.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rdio_male.Location = new System.Drawing.Point(218, 214);
+            this.rdio_male.Location = new System.Drawing.Point(290, 214);
             this.rdio_male.Name = "rdio_male";
             this.rdio_male.Size = new System.Drawing.Size(55, 21);
             this.rdio_male.TabIndex = 108;
@@ -325,7 +325,7 @@
             this.Label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.Label4.AutoSize = true;
             this.Label4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Label4.Location = new System.Drawing.Point(167, 125);
+            this.Label4.Location = new System.Drawing.Point(239, 125);
             this.Label4.Name = "Label4";
             this.Label4.Size = new System.Drawing.Size(30, 17);
             this.Label4.TabIndex = 105;
@@ -336,7 +336,7 @@
             this.Label5.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.Label5.AutoSize = true;
             this.Label5.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Label5.Location = new System.Drawing.Point(138, 153);
+            this.Label5.Location = new System.Drawing.Point(210, 153);
             this.Label5.Name = "Label5";
             this.Label5.Size = new System.Drawing.Size(63, 17);
             this.Label5.TabIndex = 107;
@@ -347,7 +347,7 @@
             this.Label9.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.Label9.AutoSize = true;
             this.Label9.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Label9.Location = new System.Drawing.Point(175, 216);
+            this.Label9.Location = new System.Drawing.Point(247, 216);
             this.Label9.Name = "Label9";
             this.Label9.Size = new System.Drawing.Size(35, 17);
             this.Label9.TabIndex = 101;
@@ -358,7 +358,7 @@
             this.btn_edit.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.btn_edit.Enabled = false;
             this.btn_edit.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_edit.Location = new System.Drawing.Point(517, 206);
+            this.btn_edit.Location = new System.Drawing.Point(589, 206);
             this.btn_edit.Name = "btn_edit";
             this.btn_edit.Size = new System.Drawing.Size(64, 36);
             this.btn_edit.TabIndex = 114;
@@ -370,7 +370,7 @@
             this.PictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.PictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.PictureBox1.Location = new System.Drawing.Point(532, 31);
+            this.PictureBox1.Location = new System.Drawing.Point(604, 31);
             this.PictureBox1.Name = "PictureBox1";
             this.PictureBox1.Size = new System.Drawing.Size(256, 169);
             this.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -381,7 +381,7 @@
             // txtPhoto
             // 
             this.txtPhoto.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.txtPhoto.Location = new System.Drawing.Point(532, 69);
+            this.txtPhoto.Location = new System.Drawing.Point(604, 69);
             this.txtPhoto.Name = "txtPhoto";
             this.txtPhoto.Size = new System.Drawing.Size(209, 20);
             this.txtPhoto.TabIndex = 122;
@@ -389,7 +389,7 @@
             // btnDisable
             // 
             this.btnDisable.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.btnDisable.Location = new System.Drawing.Point(587, 207);
+            this.btnDisable.Location = new System.Drawing.Point(659, 207);
             this.btnDisable.Name = "btnDisable";
             this.btnDisable.Size = new System.Drawing.Size(68, 36);
             this.btnDisable.TabIndex = 124;
@@ -404,7 +404,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(901, 539);
+            this.ClientSize = new System.Drawing.Size(1045, 539);
             this.Controls.Add(this.cboYearLevel);
             this.Controls.Add(this.cboCourse);
             this.Controls.Add(this.btnClose);
